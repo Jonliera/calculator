3d36ed6f29569659ceffc15dbfc20e474000aa60
Jonathan Liera <jonliera1@gmail.com>
2022-08-17 19:52:59 -0700

    add html, css and js files

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+function sub(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2){
+  return number * number2;
+}
+
+function divide(number1, number2){
+  return number1 / number2;
+}
+
+const number1 = prompt("Enter a number:");
+const number2 = prompt("Enter another number:");
+
+window.alert(add(number1, number2));
+
+window.alert(sub(number1, number2));
+
+window.alert(multiply(number1,number2));
+
+window.alert(divide(number1,numer2));
